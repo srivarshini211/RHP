e5d23c75904ceb417a8aa15545a9cc8208e74503
srivarshini211 <240155.cs@rmkec.ac.in>
2026-09-08 09:31:52 +0530

    day 22: 8/09/2026

---
+++ Moo Language School.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        string s;
+        cin>>s;
+        int f=n/k;
+        vector<bool> own(f,false);
+        for(unsigned int i=0;i<s.size();i++){
+            if(s[i]=='0'){
+                int farmidx=i/k;
+                own[farmidx]=true;
+            }
+        }
+        int count=0;
+        for(int i=0;i<f;i++){
+            if(!own[i]) count++;
+        }
+        cout<<count<<endl;
+    }
+}
